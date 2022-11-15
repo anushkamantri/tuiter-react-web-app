035b4657e4d0c314cd9832285ef8f3346d944bb1
Anushka Mantri <anushkamantri96@gmail.com>
2022-11-15 18:48:18 -0500

    a8

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -8,6 +8,8 @@ import {configureStore}
     from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
 import HomeComponent from "./home";
+import {Route, Routes} from "react-router";
+import ExploreComponent from "./explore";
 const store = configureStore(
     {reducer: {who: whoReducer, tuitsData: tuitsReducer}});
 
@@ -20,6 +22,10 @@ function Tuiter() {
                 </div>
                 <div className="col-10 col-md-10 col-lg-7 col-xl-6"
                      style={{"position": "relative"}}>
+                    <Routes>
+                        <Route path="/home" element={<HomeComponent />} />
+                        <Route path="/explore" element={<ExploreComponent />} />
+                    </Routes>
                     <HomeComponent/>
                 </div>
                 <div className="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
